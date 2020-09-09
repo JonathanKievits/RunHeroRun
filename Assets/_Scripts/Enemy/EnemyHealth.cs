9e804965ed07ecb45ec77b164b664f0384fa9ab3
Jonathan Kievits <jkievits99@gmail.com>
2020-09-09 15:27:58 +0200

    Should be able to do damage now but doing some tests if everything still works

--- Assets/_Scripts/Enemy/EnemyHealth.cs
+++ Assets/_Scripts/Enemy/EnemyHealth.cs
@@ -6,16 +6,16 @@ public class EnemyHealth : MonoBehaviour
 {
     private int _maxHealth, _currentHealth;
 
-    [SerializeField]private EnemyHealthBar HealthBar;
+    [SerializeField]private EnemyHealthBar HealthBar = null;
 
     private void Start()
     {
-        _maxHealth = 300;
+        _maxHealth = 100;
         _currentHealth = _maxHealth;
         HealthBar.SetMaxHealth(_maxHealth);
     }
 
-    private void GetDamage(int damage)
+    public void DoDamage(int damage)
     {
         _currentHealth -= damage;
         HealthBar.SetHealth(_currentHealth);
--- Assets/_Scripts/MovementDetection.cs
+++ Assets/_Scripts/MovementDetection.cs
@@ -6,23 +6,30 @@ using PedometerU;
 
 public class MovementDetection : MonoBehaviour
 {
-    public Text _stepText, _distanceText;
+    public Text StepText;
     private Pedometer _pedometer;
+    private EnemyHealth _enemyHealth = null;
+
+    private void Awake()
+    {
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+    }
 
     private void Start()
     {
          _pedometer = new Pedometer(OnStep);
         OnStep(0, 0);
+        _enemyHealth.GetComponent<EnemyHealth>();
     }
 
     private void OnStep(int _steps, double _distance)
     {
-        _stepText.text = _steps.ToString();
-        _distanceText.text = _distance.ToString();
+        _enemyHealth.DoDamage(_steps);
+        StepText.text = (_steps.ToString()) + " " + (_distance.ToString("2F"));
     }
 
-    public void WhenDone()
+    /*public void WhenDone()
     {
         _pedometer.Dispose();
-    }
+    }*/
 }
