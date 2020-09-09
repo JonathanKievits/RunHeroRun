863282b0bbcc2963c7724abc9e62ba5fe7a4cd1a
Jonathan Kievits <jkievits99@gmail.com>
2020-09-09 16:43:24 +0200

    Making a safety commit to check where things broke with the pedometer as it still works here

---
+++ Assets/_Scripts/Enemy/EnemyHealth.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    private int _maxHealth, _currentHealth;
+
+    [SerializeField] private EnemyHealthBar HealthBar;
+
+    private void Start()
+    {
+        _maxHealth = 300;
+        _currentHealth = _maxHealth;
+        HealthBar.SetMaxHealth(_maxHealth);
+    }
+
+    private void GetDamage(int damage)
+    {
+        _currentHealth -= damage;
+        HealthBar.SetHealth(_currentHealth);
+    }
+}
