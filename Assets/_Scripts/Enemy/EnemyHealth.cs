dd0c4e81386637e0c9aab7c7ea80604ac0bcd3cc
Jonathan Kievits <jkievits99@gmail.com>
2020-09-15 20:04:07 +0200

    Added some audiofiles and sound is playing when hitting an enemy and in the background

--- Assets/_Scripts/Enemy/EnemyHealth.cs
+++ Assets/_Scripts/Enemy/EnemyHealth.cs
@@ -6,18 +6,36 @@ public class EnemyHealth : MonoBehaviour
 {
     private int _maxHealth, _currentHealth;
 
-    [SerializeField] private EnemyHealthBar HealthBar;
+    [SerializeField] private EnemyHealthBar _healthBar;
 
     private void Start()
     {
         _maxHealth = 500;
         _currentHealth = _maxHealth;
-        HealthBar.SetMaxHealth(_maxHealth);
+        _healthBar.SetMaxHealth(_maxHealth);
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            DoDamage(25);
+        }
+    }
+    
     public void DoDamage(int damage)
     {
         _currentHealth -= damage;
-        HealthBar.SetHealth(_currentHealth);
+        _healthBar.SetHealth(_currentHealth);
+        if(_currentHealth <= 0)
+        {
+            //Plays a sound when the enemy has no HP left and destroys the enemy
+            FindObjectOfType<PlayerAudio>().PlaySound("DeadEnemy");
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            FindObjectOfType<PlayerAudio>().PlaySound("HitEnemy");
+        }
     }
 }
